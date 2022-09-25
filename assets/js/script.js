f4628554f6abb51858877b1960f5acad0f4c7939
sundayswellB356 <sundayswellB356@outlook.com>
2022-09-25 14:37:35 +0000

    "Add all changes"

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+/**The main game "loop"is first loaded and after the user's answer has been processed */
 function runGame(){
 let num1=Math.floor(Math.random()*25)+1;
 let num2=Math.floor(Math.random()*25)+1;
