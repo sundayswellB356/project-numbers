1e468f8ea7a7ec92a0dd04951b32d58fe9d95d75
sundayswellB356 <sundayswellB356@outlook.com>
2022-09-25 14:58:57 +0000

    "Add all changes"

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,30 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 /**The main game "loop"is first loaded and after the user's answer has been processed */
-function runGame(){
+function runGame(gameType){
 let num1=Math.floor(Math.random()*25)+1;
 let num2=Math.floor(Math.random()*25)+1;
 
 }
 
-function checkAnswer(){
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
+    function displayAdditionQuestion(operand1,operand2){
+        document.getElementById(operand1`).textContent=operand1;
+    }
+    function displayAdditionQuestion(operand2,){
+        document.getElementById(operand2`).textContent=operand2;
+    }
+
+document.getElementById('operator').textContent="+";
+
+    function checkAnswer(){
 
 }
 
