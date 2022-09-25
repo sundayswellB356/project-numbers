adf93b0c4cb086a9aedc6dd13ea846b6e2854a45
sundayswellB356 <sundayswellB356@outlook.com>
2022-09-25 15:00:55 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,21 +27,20 @@ let num2=Math.floor(Math.random()*25)+1;
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
-    function displayAdditionQuestion(operand1,operand2){
-        document.getElementById(operand1`).textContent=operand1;
-    }
-    function displayAdditionQuestion(operand2,){
-        document.getElementById(operand2`).textContent=operand2;
-    }
-
-document.getElementById('operator').textContent="+";
+    
 
     function checkAnswer(){
 
 }
 
 function calculateCorrectAnswer(){
+    function displayAdditionQuestion(operand1, operand2) {
 
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
+        
+    }
 }
 
 function IcrementScore(){
