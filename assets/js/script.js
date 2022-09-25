9ec17542e04bca12ca1d0ac5c5404350c7edd8c5
sundayswellB356 <sundayswellB356@outlook.com>
2022-09-25 13:14:16 +0000

    Setup structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function IcrementScore(){
+
+}
+
+function IcrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
